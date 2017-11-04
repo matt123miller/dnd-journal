4387cefa00d03014c1305949cf18a13bd795da9e
Matt Mac <mmiller@tfspark.com>
2017-11-04 15:25:54 +0000

    Updated the code to count each files words and total the amount. I could've made some recursive function but honestly there was no point so I hardcoded the 8 paths.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import sys
 import os
-import re
 
 '''
 A script to count all my words, because I can't help myself.
 Read anything in the format 'game-n' where n is an integer of 0 or more
 So it should read folders and also files.
 I want to try and avoid the -notes files.
+Maybe some other time....
 '''
+
+filePaths = ['game-1/game-1.md', 'game-2/game-2.md', 'game-3/game-3.md', 'game-4/game-4.md',
+             'game-5/game-5.md', 'game-6/game-6.md', 'game-7/game-7.md', 'game-8/game-8.md']
 
 
 def main():
-    with open('game-1/game-1.md', mode='r') as rawFile:
-        mdFile = rawFile.read()
-        words = word_count(mdFile)
-        print(words)
+    wordTotal = 0
+
+    for path in filePaths:
+        with open(path, mode='r') as rawFile:
+            mdFile = rawFile.read()
+            wordTotal += word_count(mdFile)
+
+    print(wordTotal)
 
 
 def word_count(mdFile):
